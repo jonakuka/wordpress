235b1b6380b02152d17cddc46c1cdd7d93c511cd
jonakuka <jonakuka123@gmail.com>
2025-09-09 20:40:37 +0200

    day 6

---
+++ lesson  6/Untitled-1.php
@@ -0,0 +1,9 @@
+<?php
+   function ds_enqueue_scripts() {
+       wp_enqueue_script('jquery');
+   }
+   add_action('wp_enqueue_scripts', 'ds_enqueue_scripts');<?php
+   function ds_enqueue_scripts() {
+       wp_enqueue_script('jquery');
+   }
+   add_action('wp_enqueue_scripts', 'ds_enqueue_scripts');
